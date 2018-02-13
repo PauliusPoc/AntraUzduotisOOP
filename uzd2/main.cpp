abe87068cb4359b6445b72e5d57b3c64b8943088
Augustinas Makevičius <augustinas.makevicius@mif.stud.vu.lt>
2018-02-13 21:23:14 +0200

    Pabaigta rašyti main.cpp, bei paruošta v.1 versijai

--- uzd2/main.cpp
+++ uzd2/main.cpp
@@ -1,45 +1,105 @@
-#include <iostream>
-#include <vector>
+#include <algorithm>
 #include <iomanip>
+#include <iostream>
+#include <random>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
 using std::vector;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::sort;
+
+void Auto(int &egzam, vector<int> &pazym);
+void ByHand(int &egzam, vector<int> &pazymiai);
+double PagalVidurki(int egzam, vector<int> &pazymiai);
+double PagalMediana(int egzam, vector<int> &pazymiai);
 
 
 int main() {
 
 
     cout << "Ivesk studento varda ir pavarde: ";
-    string varpav;
+    string varpav{};
     cin >> varpav;
 
-    cout << "Ivesk egzamino rezultata: ";
-    double egzam{};
-    cin >> egzam;
+    cout << "Pasirinkite funkcija. 1 - pazymius generuok automatiskai, 2 - pazymius vesti ranka: ";
+    int choice{}, metod{};
+    cin >> choice;
+    cout << "Pasirinkite funkcija. 1 - skaiciuk pagal vidurki, 2 - skaiciuk pagal mediana: ";
+    cin >> metod;
 
-    vector<double> pazymiai;
-    double  pazym;
-    do{
-        cin >> pazym;
-        if(pazym) pazymiai.push_back(pazym);
-    }while (pazym);
+    int egzam{};
+    vector<int> pazymiai{};
 
-    double  suma{};
-    for(auto paz : pazymiai){
-        suma += paz;
-    }
+    if (choice == 1) Auto(egzam,pazymiai);
+    else ByHand(egzam,pazymiai);
 
     double galut{};
-    galut = 0.4 * suma/pazymiai.size() +  0.6 * egzam;
+    if (metod == 1) galut =  PagalVidurki(egzam,pazymiai);
+    else galut = PagalMediana(egzam,pazymiai);
+
 
     cout << endl << endl << endl << varpav << endl << "Pazymiai: " << endl;
     for(auto paz : pazymiai){
         cout << paz << endl;
     }
-    cout << std::fixed << std::setprecision(2) << "Egzaminas - " << egzam << endl << "Galutinis - " << galut << endl;
+    cout << "Egzaminas - " << egzam << endl << std::fixed << std::setprecision(2) << "Galutinis - " << galut << endl;
 
     return 0;
 }
+
+void Auto(int &egzam, vector<int> &pazym) {
+
+    cout << "Kiek pazymiu generuoti? ";
+    int kiekis{};
+    cin >> kiekis;
+
+    random_device rd;
+    mt19937 mt(rd() * time(0) + rd());
+    uniform_int_distribution<int> dist(1,10);
+
+    egzam = dist(mt);
+    for (int i = 0; i < kiekis; i++){
+
+        pazym.push_back(dist(mt));
+    }
+}
+
+void ByHand(int &egzam, vector<int> &pazymiai) {
+
+    cout << "Ivesk egzamino rezultata: ";
+    cin >> egzam;
+
+    int  pazym = 0;
+    do{
+        cout << "Ivesk pazymi arba bet koki kita simboli, noredamas uzbaigti ivedima: ";
+        cin >> pazym;
+        if(pazym) pazymiai.push_back(pazym);
+    }while (pazym);
+}
+
+double PagalVidurki(int egzam, vector<int> &pazymiai) {
+
+    double  suma{};
+    sort(pazymiai.begin(), pazymiai.end());
+    for(auto paz : pazymiai){
+        suma += paz;
+    }
+
+    return 0.4 * suma/pazymiai.size() +  0.6 * egzam;
+
+}
+
+double PagalMediana(int egzam, vector<int> &pazymiai) {
+    double med{};
+    sort(pazymiai.begin(), pazymiai.end());
+    med = pazymiai.size() % 2 == 0 ? (pazymiai[pazymiai.size() / 2] + pazymiai[pazymiai.size() / 2 - 1]) / 2
+                                   : pazymiai[pazymiai.size() / 2];
+    return 0.4 * med +  0.6 * egzam;
+}
+
+
