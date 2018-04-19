c28eb918459c7aa95825a5f54710a1b6d86fe62b
Makevičius Augustinas <augustinas.makevicius@mif.stud.vu.lt>
2018-04-19 23:49:57 +0300

    Configų atnaujinimas

--- AntraUzduotis/cpp/main.cpp
+++ AntraUzduotis/cpp/main.cpp
@@ -1,25 +1,22 @@
-#include "../headers/DarbasKonsole.h"
 #include "../headers/Studentas.h"
 #include "../headers/DarbasFailai.h"
 #include "../headers/Performance.h"
 
-void Konsole();
 void DarbasSuFailais();
 void Performance();
 
 int main() {
     f1:
     int fileOrNot;
-    if (cout << "Pasirinkite funkcija. 1 - duomenis rink is failo, 2 - duomenis rink is konsoles, 3 - performance profiler: " && !(cin >> fileOrNot)) {
+    if (cout << "Pasirinkite funkcija.\n1 - duomenis rink is failo (v0.2),\n2 - performance profiler: " && !(cin >> fileOrNot)) {
         cin.clear(); //clear bad input flag
         cin.ignore(::std::numeric_limits<int>::max(), '\n'); //discard input
         cout << "Neteisinga įvestis\n";
         goto f1;
     }
-    if (fileOrNot >= 1 && fileOrNot <= 3) {
+    if (fileOrNot >= 1 && fileOrNot <= 2) {
 
         if (fileOrNot == 1) DarbasSuFailais();
-        else if (fileOrNot == 2) Konsole();
         else Performance();
     } else {
         cout << "Toks pasirinkimas negalimas" << endl;
@@ -32,15 +29,8 @@ void DarbasSuFailais() {
     Nuskaitymas(kolegos,"kursiokai.txt");
     Rasymas(kolegos);
 }
-void Konsole() {
-    unsigned int choice{}, metod{};
-    Choices(choice, metod);
 
-    Studentas S;
-    S.readFromKonsole(cin, choice);
-    S.printToKonsole(cout, metod);
-}
 void Performance(){
-    const unsigned int nTestu = 4; // kiek testų  atlkti?
+    const unsigned int nTestu = 6; // kiek testų  atlkti?
     StartTesting(nTestu);
 }
