71c5fcdf8734e329bc4616ec5907fdf52016ea82
Makevičius Augustinas <augustinas.makevicius@mif.stud.vu.lt>
2018-04-24 22:34:23 +0300

    Doxygen on Performance.h

--- AntraUzduotis/headers/Performance.h
+++ AntraUzduotis/headers/Performance.h
@@ -22,18 +22,37 @@ using std::cin;
 using std::string;
 using std::vector;
 
+/// Funkcija, tikrinanti ar studento gal. balas >= 5, pasitelkus vidurkį
 bool RibaV (Studentas& val);
+/// Funkcija, tikrinanti ar studento gal. balas >= 5, pasitelkus medianą
 bool RibaM (Studentas& val);
 
+/**
+ * Funckciją nustatanti parametrus ir vykdanti profiliavimą
+ * @param nTestu vykdomų testų skaičius
+ */
 void StartTesting(const unsigned int nTestu);
-
+/**
+ * Funkcija, generuojanti atsitiktinius duomenis profiliavimui
+ * @param n  generuojamų įrašų skaičius
+ * @param fk išvesties strem'as
+ */
 void GeneruokTestui(unsigned int n, std::ofstream &fk);
 
+/**
+ * Profiliavimo funkciją
+ * @param n 10 laipsnio rodiklis (kiek įrašų bus faile = 10 ^ n)
+ * @param met  Profiliavimo metodo numeris (vidurkis ar mediana)
+ */
 void StartProfiling(unsigned int n, const unsigned int met);
 
-template<typename T>
-void ArKietas(T &koleg, T &geek, T &lose, bool arVidurkiu);
-
+/**
+ * Rūšiavimo funkcija
+ * @tparam T Studentų konteinerio tipas
+ * @param koleg Studentų konteineris
+ * @param geek  gerų Studentų konteinerio
+ * @param arVidurkiu Profiliavimo metodas(vidurkis(true) ar mediana(false))
+ */
 template<typename T>
 void ArKietas(T &koleg, T &geek, bool arVidurkiu);
 
