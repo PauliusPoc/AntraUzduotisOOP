a8c72f2278e285d835884e4c9aadc1fee6cb1b2e
Makevičius Augustinas <augustinas.makevicius@mif.stud.vu.lt>
2018-04-24 22:40:41 +0300

    Doxygen Final

--- AntraUzduotis/headers/DarbasFailai.h
+++ AntraUzduotis/headers/DarbasFailai.h
@@ -10,8 +10,16 @@
 #include <list>
 #include <deque>
 
-
+/**
+ * Funkcija nuskaitanti Studentų duomenis iš failo
+ * @param kolegos Studentų konteineris
+ * @param s duomenų failo pavadinimas
+ */
 void Nuskaitymas(vector<Studentas> &kolegos, string s);
+/**
+ * Funkcija išrašanti studentus
+ * @param kolegos Studentų konteineris
+ */
 void Rasymas(vector<Studentas> &kolegos);
 
 #endif //ANTRAUZDUOTIS_DARBASFAILAI_H
