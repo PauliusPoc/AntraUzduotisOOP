f169d05e67b351495ab53c9a88e1dec68900eba5
Makevičius Augustinas <augustinas.makevicius@mif.stud.vu.lt>
2018-04-24 22:35:41 +0300

    Doxygen on Studentas.h

--- AntraUzduotis/headers/Studentas.h
+++ AntraUzduotis/headers/Studentas.h
@@ -21,27 +21,80 @@ using std::ostream;
 using std::endl;
 using std::cout;
 
+typedef double(*fun)();
+/// Studentus apibrėžianti klasė
 class Studentas {
-    private:
-        string vardas_{};
-        string pavard_{};
-        vector<double> nDarbai_{};
-        double egzam_{};
-    public:
-        Studentas() : egzam_(0){}
+private:
+    /// Studento vardas
+    string vardas_{};
+    /// Studnto pavardė
+    string pavard_{};
+    /// Studento namų darbų įverčiai
+    vector<double> nDarbai_{};
+    /// Studento gautas egzamino įvertinimas
+    double egzam_;
+public:
+    /**
+     * Sukonsyruoja studento objektą
+     */
+    Studentas() : egzam_(0){}
 
-        inline string vardas() const { return vardas_; }
-        inline string pavarde() const { return pavard_; }
+    /**
+     * Gražinamas studento vardas
+     * @return Studento vardas
+     */
+    inline string vardas() const { return vardas_; }
 
-        void print(unsigned int, unsigned int, std::ostream&);
-        friend istream &operator>>( istream&, Studentas& );
-        friend ostream &operator<<( ostream&, Studentas&);
-        double vidurkis() const;
-        double mediana();
+    /**
+     * Gražinama studento pavardė
+     * @return Studento pavardė
+     */
+    inline string pavarde() const { return pavard_; }
+
+    /**
+    * Nuskaito studento informaciją iš įvesties stream'o
+    * @return input stream'as
+    */
+    friend istream &operator>>( istream&, Studentas& );
+
+    /**
+     *  Studento informacijos išvedimas lygiuojant duomenis
+     * @param dv Studento, kurio vardas ilgiausias, vardo ilgis
+     * @param dp Studento, kurio pavardė ilgiausia, pavardės ilgis
+     * @param fr išvesties stream'as
+     */
+    void print(unsigned int dv, unsigned int dp, std::ostream &fr);
+
+    /**
+     * Studento informacijos išvedimas
+     * @return išvesties stream'as
+    */
+    friend ostream &operator<<( ostream&, Studentas&);
+
+    /**
+    * Suskaičiuoja namų darbų įvertinimų vidurkį
+    * @return namų darbų įvertinimų vidurkis
+    */
+    double vidurkis() const;
+
+    /**
+    * Suskaičiuoja namų darbų įvertinimų medianą
+    * @return namų darbų įvertinimų mediana
+    */
+    double mediana();
+
+    /**
+     * Skaičiuojamas studento galutinis balas
+     * @param F Funkcija pagal kurią skaičiuojame balą (vidurkis arba mediana)
+     * @return Studento galutinis balas
+     */
+    double galBalas(fun F);
+
 };
-
+/**
+ * Lygina du studentus pagal vardus ir pavardes abeceliškai
+ * @return true, jei pirmo studento vardas ir pavarde yra pirmesni pagal abecele, false - jei ne
+ */
 bool compare(const Studentas&, const Studentas&);
-
-
 
 #endif //ANTRAUZDUOTIS_STUDENTAS_H
